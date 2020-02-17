cb979d47faeda7f7edb0ad13329e92f2c3384d00
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2020-02-17 15:40:39 -0500

    countdown timer 8

--- client/App.jsx
+++ client/App.jsx
@@ -10,34 +10,7 @@ const App = () => {
     const [countdown, setCountdown] = useState('')
     const [countdownTimer, setCountdownTimer] = useState('')
 
-    //******************* */
 
-    var countDownDate = countdown.getTime();
-
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-    
-      // Get today's date and time
-      var now = new Date().getTime();
-        
-      // Find the distance between now and the count down date
-      var distance = countDownDate - now;
-        
-      // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-   
-     setCountdownTimer(minutes,':',seconds)
-      // If the count down is over, write some text 
-      if (distance < 0) {
-        clearInterval(x);
-        //document.getElementById("demo").innerHTML = "EXPIRED";
-      }
-    }, 1000);
-
-    //********************************************* */
 
     useEffect(() => {
         rstream.on('getMobilesFromServer', (mobileArray) => {
@@ -45,6 +18,34 @@ const App = () => {
         })
         rstream.on('countdown', countdown_ => {
             setCountdown(countdown_.toISOString())
+            //******************* */
+
+            var countDownDate = new Date(countdown).getTime();
+
+            // Update the count down every 1 second
+            var x = setInterval(function () {
+
+                // Get today's date and time
+                var now = new Date().getTime();
+
+                // Find the distance between now and the count down date
+                var distance = countDownDate - now;
+
+                // Time calculations for days, hours, minutes and seconds
+                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                setCountdownTimer(minutes, ':', seconds)
+                // If the count down is over, write some text 
+                if (distance < 0) {
+                    clearInterval(x);
+                    //document.getElementById("demo").innerHTML = "EXPIRED";
+                }
+            }, 1000);
+
+            //********************************************* */
         })
     }, [])
     const sendCommand = (mobil) => {
