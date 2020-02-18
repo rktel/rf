188640cba545b5aa52c8136a08e14526294bb5fa
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2020-02-18 10:21:30 -0500

    client grid 10

--- client/App.jsx
+++ client/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
             <div className="pure-g">
                 <div className="pure-u-1-3" style={{ 'background': 'gray' }}>
                     <h4>Countdown Time {countdown} </h4>
-                    <table className="pure-table" style={{ 'height': 'calc(100vh-400)' }}>
+                    <table className="pure-table" style={{ height: 'calc(100%-400)' }}>
                         <thead>
                             <tr>
                                 <th>#</th>
