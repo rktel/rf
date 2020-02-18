15dbe62561abc39146e65e4be6eb181248b9cd1c
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2020-02-18 10:16:51 -0500

    client grid 9

--- client/App.jsx
+++ client/App.jsx
@@ -17,11 +17,11 @@ const App = () => {
         rstream.emit('writeCommand', mobil, '>QVR<')
     }
     return (
-        <div style={{ 'maxHeight': '500px' }}>
+        <div>
             <div className="pure-g">
                 <div className="pure-u-1-3" style={{ 'background': 'gray' }}>
                     <h4>Countdown Time {countdown} </h4>
-                    <table className="pure-table">
+                    <table className="pure-table" style={{ 'height': 'calc(100vh-400)' }}>
                         <thead>
                             <tr>
                                 <th>#</th>
