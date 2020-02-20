1becb85a9c9b3314827ad733ea2d759d4bae8553
rktel <rakinakuy@hotmail.com>
2020-02-20 15:47:52 -0500

    add framework7

--- client/App.jsx
+++ client/App.jsx
@@ -1,131 +1,27 @@
-import g from '../imports/tools/log'
-import React, { useState, useEffect, useRef } from 'react'
-import { rstream } from '../imports/api/streamers'
+// App.jsx
+// Import React
+import React from  'react';// App.jsx
+import { App, View, Page, Navbar, Toolbar, Link } from 'framework7-react';
 
-const App = () => {
-    const [mobiles, setMobiles] = useState([])
-    const [countdown, setCountdown] = useState('')
-    const [mobileTextFilter, setMobileTextFilter] = useState('')
-    const [mobilesSelected, setMobilesSelected] = useState([])
-    const [messageText, setMessageText] = useState('')
+export default () => (
+  // Main Framework7 App component where we pass Framework7 params
+  <App params={{ theme: 'auto', name: 'My App', id: 'com.demoapp.test' }}>
 
-    const handleOnChangeMobileTextFilter = (event) => setMobileTextFilter(event.target.value)
-    const handleOnClickCleanButton = () => setMobileTextFilter('')
-    const handleOnClickAddButton = (mobil) => {
-        if (mobilesSelected.indexOf(mobil) < 0) {
-            setMobilesSelected([...mobilesSelected, mobil])
-        } else {
-            setMobilesSelected(mobilesSelected.filter(el => el != mobil))
-        }
-    }
-    const handleOnChangeMessageText = (event) => setMessageText(event.target.value)
-    const handleOnClickSendButton = () => {
-        mobilesSelected.forEach(mobil => {
-            rstream.emit('writeCommand', mobil, messageText)
-        })
-    }
-    const handleOnClickCleanMessageButton = () => setMessageText('')
-
-
-    useEffect(() => {
-        rstream.on('getMobilesFromServer', (mobileArray) => {
-            setMobiles(mobileArray)
-        })
-        rstream.on('countdown', countdown_ => {
-            setCountdown(new Date(countdown_).addHours(-5).toISOString().split('T')[1])
-        })
-    }, [])
-
-    const sendCommand = (mobil) => {
-        rstream.emit('writeCommand', mobil, '>QVR<')
-    }
-
-
-
-    const MobilesItems = () => {
-        return (mobiles.filter(el => el.mobileID.indexOf(mobileTextFilter) >= 0)
-            .map((mobil, index) =>
-                <tr key={mobil.mobileID}>
-                    <td>{index + 1} </td>
-                    <td>{mobil.mobileID}</td>
-                    <td><button onClick={() => handleOnClickAddButton(mobil.mobileID)}>ADD</button></td>
-                </tr>))
-    }
-    const MobileSelectedItems = () => {
-        return (mobilesSelected.map((mobil, index) =>
-            <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{mobil}</td>
-                <td><button onClick={() => handleOnClickAddButton(mobil)}>DEL</button></td>
-            </tr>
-        ))
-    }
-    const MessageDrywall = () => {
-        return (
-            <div>
-                <h3>Message</h3>
-
-            </div>
-        )
-    }
-    const ScriptDrywall = () => {
-        return (
-            <div>
-                <h3>Script</h3>
-            </div>
-        )
-    }
-    return (
-        <div>
-            <div className="flex-container">
-                <div className="flex-item" style={{ 'background': 'gray' }}>
-                    <h4>Countdown Time {countdown} </h4>
-                    <input placeholder="Buscar IMEI" id="mobileTextFilter" type="text" onChange={handleOnChangeMobileTextFilter} value={mobileTextFilter} />
-                    <button onClick={handleOnClickCleanButton}>CLEAN</button>
-                    <table >
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>IMEI</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <MobilesItems />
-                        </tbody>
-                    </table>
-                    <br />
-                </div>
-                <div className="flex-item" style={{ 'background': 'cornflowerblue' }}>
-                    <h4>Selected devices</h4>
-                    <table >
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>IMEI</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <MobileSelectedItems />
-                        </tbody>
-                    </table>
-                    <br />
-
-                </div>
-                <div className="flex-item" style={{ 'background': 'goldenrod' }}>
-                    <input placeholder="Message" type="text" id="messageText" onChange={handleOnChangeMessageText} value={messageText} />
-                    <button onClick={handleOnClickCleanMessageButton}>CLEAN</button>
-                    <button onClick={handleOnClickSendButton}>SEND</button>
-                </div>
-            </div>
-        </div >
-    )
-}
-
-export default App
-
-Date.prototype.addHours = function (h) {
-    this.setTime(this.getTime() + (h * 60 * 60 * 1000));
-    return this;
-}
+    {/* Your main view, should have "main" prop */}
+    <View main>
+      {/*  Initial Page */}
+      <Page>
+        {/* Top Navbar */}
+        <Navbar title="Awesome App"></Navbar>
+        {/* Toolbar */}
+        <Toolbar bottom>
+          <Link>Link 1</Link>
+          <Link>Link 2</Link>
+        </Toolbar>
+        {/* Page Content */}
+        <p>Page content goes here</p>
+        <Link href="/about/">About App</Link>
+      </Page>
+    </View>
+  </App>
+)
--- client/index.js
+++ client/index.js
@@ -1,5 +1,24 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './App'
 
-render(<App />, document.getElementById('root'))
+// Import React
+import React from  'react';
+
+// Import ReactDOM
+import ReactDOM from  'react-dom';
+
+// Import F7 Bundle
+import Framework7 from 'framework7/framework7-lite.esm.bundle.js';
+
+// Import F7-React Plugin
+import Framework7React from 'framework7-react';
+
+// Init F7-React Plugin
+Framework7.use(Framework7React);
+
+// Import Main App component
+import App from './App.jsx';
+
+// Mount React App
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById('root')
+)
