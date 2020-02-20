b4b738f662b0f4a1ad4966a409b29212c0d98660
rktel <rakinakuy@hotmail.com>
2020-02-20 16:05:01 -0500

    add framework7 2

--- client/App.jsx
+++ client/App.jsx
@@ -5,7 +5,7 @@ import { App, View, Page, Navbar, Toolbar, Link } from 'framework7-react';
 
 export default () => (
   // Main Framework7 App component where we pass Framework7 params
-  <App params={{ theme: 'auto', name: 'My App', id: 'com.demoapp.test' }}>
+  <App params={{ theme: 'auto', name: 'My App', id: 'com.pe.rktel' }}>
 
     {/* Your main view, should have "main" prop */}
     <View main>
@@ -14,7 +14,7 @@ export default () => (
         {/* Top Navbar */}
         <Navbar title="Awesome App"></Navbar>
         {/* Toolbar */}
-        <Toolbar bottom>
+        <Toolbar>
           <Link>Link 1</Link>
           <Link>Link 2</Link>
         </Toolbar>
