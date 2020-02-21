05df73cbb02f6e7dda0cbc699b05dffe198d66b3
rktel <rakinakuy@hotmail.com>
2020-02-21 13:51:56 -0500

    client list mobiles 7

--- client/App.jsx
+++ client/App.jsx
@@ -44,7 +44,7 @@ export default () => {
                                     <ListItem key={mobil.mobileID}>
                                         <strong>{index + 1}</strong> =>
                                         <span>{mobil.mobileID}</span>
-                                        <Toggle  color="green" change={e=>g(e, mobil)}/>
+                                        <Toggle  color="green" onChange={e=>g(e, mobil)}/>
                                     </ListItem>
                                 ))}
                             </List>
