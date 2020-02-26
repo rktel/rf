bb78e3ce3642837d8a5edb96c98fb5083249f489
rktel <rakinakuy@hotmail.com>
2020-02-26 16:48:33 -0500

    now Svelte

---
+++ client/App.svelte
@@ -0,0 +1,1 @@
+<h1>Peppa Pig</h1>
--- client/index.js
+++ client/index.js
@@ -1,24 +1,5 @@
+import App from './App.svelte'
 
-// Import React
-import React from  'react';
-
-// Import ReactDOM
-import ReactDOM from  'react-dom';
-
-// Import F7 Bundle
-import Framework7 from 'framework7/framework7-lite.esm.bundle.js';
-
-// Import F7-React Plugin
-import Framework7React from 'framework7-react';
-
-// Init F7-React Plugin
-Framework7.use(Framework7React);
-
-// Import Main App component
-import App from './App.jsx';
-
-// Mount React App
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById('root')
-)
+new App({
+    target: document.body
+})
